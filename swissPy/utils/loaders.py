07c9ebafefbbb9b83e2391de3f3a3f052679b0f7
awakenedhaki <rodrigo.vallejosch@gmail.com>
2019-08-30 17:25:15 -0700

    Paths look two parents above

--- swissPy/utils/loaders.py
+++ swissPy/utils/loaders.py
@@ -4,18 +4,18 @@ from pathlib import Path
 
 
 def load_extensions():
-    EXTENSIONS = Path(__file__).parent / 'jsons' / 'extensions.json'
+    EXTENSIONS = Path(__file__).parents[1] / 'jsons' / 'extensions.json'
     with EXTENSIONS.open('r') as f:
         return json.load(f)
 
 
 def load_problems():
-    PROBLEMS = Path(__file__).parent / 'jsons' / 'problems.json'
+    PROBLEMS = Path(__file__).parents[1] / 'jsons' / 'problems.json'
     with PROBLEMS.open('r') as f:
         return json.load(f)
 
 
 def load_template():
-    TEMPLATES = Path(__file__).parent / 'jsons' / 'templates.json'
+    TEMPLATES = Path(__file__).parents[1] / 'jsons' / 'templates.json'
     with TEMPLATES.open('r') as f:
         return json.load(f)
