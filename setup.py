11d584a670f8092f8d0cce08c27bc960dca40041
awakenedhaki <rodrigo.vallejosch@gmail.com>
2019-08-30 17:42:27 -0700

    Creating distribution files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+setuptools.setup(
+    name="awakenedhaki",
+    version="0.1.0",
+    author="Rodrigo Vallejos",
+    description="Project Euler facilitator CLI",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords=['swisspy', 'swissPy', 'euler', 'project-euler', 'projecteuler'],
+    license="MIT",
+    classifiers=[
+        "Topic :: Utilities",
+    ],
+    url="https://github.com/awakenedhaki/swissPy",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
